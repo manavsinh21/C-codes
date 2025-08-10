2a319c3c195d6b27a049f2d28b0489cb1eccd2d1
manavsinh21 <manav.sindha07@gmail.com>
2025-08-10 11:33:13 +0530

    Add files via upload

---
+++ multiplication of two numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    int a, b, c;
+    printf("Enter a");
+    scanf("%d", &a);
+
+    printf("Enter b");
+    scanf("%d", &b);
+
+    c= a*b;
+    printf("Product of %d and %d is %d\n", a, b, c);
+    return 0;
+    
+
+}
