a01661a12f066bbe30f1eaefd927dd519875135e
manavsinh21 <manav.sindha07@gmail.com>
2025-08-14 14:33:15 +0530

    Add files via upload

---
+++ ifst.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+
+int a;
+
+printf("Enter a number");
+scanf("%d", &a);
+
+if ( a > 9 )
+printf("it is a 2 digit number");
+
+else("number is single digit");
+return 0;
+
+}
+
+
+
