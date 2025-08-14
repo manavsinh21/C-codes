31227634ebfa2afa70bb5be8dab67e5209462955
manavsinh21 <manav.sindha07@gmail.com>
2025-08-14 15:08:18 +0530

    Add files via upload

---
+++ largest_smallest.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+int a,b;
+printf("Enter a");
+scanf("%d", &a);
+
+printf("Enter b");
+scanf("%d", &b);
+
+if (a>b){
+printf("a is greater than b");
+}
+else {
+printf("a is smaller than b");
+return 0;
+}
+}
